c51e856957317be9184ce95b7ab7ed8edf5fb539
pedrodev16 <pedrodev1.6@gmail.com>
2025-05-13 01:21:09 -0400

    test5

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   output: 'export',
-};
 
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
 
-export default nextConfig;
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+
+module.exports = nextConfig
