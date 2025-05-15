bb56049ff2f08d3480c798c16c602998b4839bd2
pedrodev16 <pedrodev1.6@gmail.com>
2025-05-15 12:21:37 -0400

    testgoogle3

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import { FC, PropsWithChildren } from "react";
 import '@/app/ui/globals.css';
 import { SessionProvider } from "next-auth/react";
+
 const RootLayout: FC<PropsWithChildren> = ({ children }: PropsWithChildren) => {
     return (
         <html lang="en">
--- app/lib/auth.js
+++ app/lib/auth.js
@@ -1,34 +1,6 @@
 import { signIn, signOut } from 'next-auth/react';
-import { useSession } from 'next-auth/react';
-import { Set_usuario_auth, existeIdGoogle } from '../database/models/Set_usuario_auth';
+import { GuardarIdGoogle } from './user';
 export const handleSignIn = () => signIn('google');
 export const handleSignOut = () => signOut();
 
-
-
-//guardar id_google en la base de datos mysql
-
-export default function GuardarIdGoogle() {
-    const { data: session } = useSession();
-
-    useEffect(() => {
-        if (session) {
-            const id_google = session.user.id;
-            const nombre = session.user.name;
-            const email = session.user.email;
-
-            // Verificar si el ID de Google ya existe en la base de datos
-            existeIdGoogle(id_google).then((existe) => {
-                if (!existe) {
-                    Set_usuario_auth(nombre, email, id_google)
-                        .then((response) => console.log('ID de Google guardado:', response))
-                        .catch((error) => console.error('Error al guardar el ID:', error));
-                } else {
-                    console.log('El ID de Google ya existe en la base de datos');
-                }
-            });
-        }
-    }, [session]);
-
-    return null; // Este componente no renderiza nada, solo ejecuta la lógica
-}
+GuardarIdGoogle();
