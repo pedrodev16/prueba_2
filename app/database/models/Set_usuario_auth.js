bb9a9ab4513440c69f6cd61fdaa9347061412b8e
pedrodev16 <pedrodev1.6@gmail.com>
2025-05-15 10:24:42 -0400

    testgoogle

---
+++ app/database/models/Set_usuario_auth.js
@@ -0,0 +1,33 @@
+'use server';
+import { createConnection } from '../conexion';
+
+export async function Set_usuario_auth(nombre, apellido, email, telefono, auth_google_id) {
+    const connection = await createConnection();
+
+    try {
+        const [result] = await connection.query(
+            'INSERT INTO `rifas` (nombre, apellido, email, telefono, auth_google_id) VALUES (?, ?, ?, ?, ?)',
+            [nombre, apellido, email, telefono, auth_google_id]
+        );
+        return { insertId: result.insertId };
+    } catch (error) {
+        return { error: error.message };
+    } finally {
+        connection.end();
+    }
+}
+export async function existeIdGoogle(id_google) {
+    const connection = await createConnection();
+
+    try {
+        const [rows] = await connection.query(
+            'SELECT * FROM `rifas` WHERE auth_google_id = ?',
+            [id_google]
+        );
+        return rows;
+    } catch (error) {
+        return { error: error.message };
+    } finally {
+        connection.end();
+    }
+}
