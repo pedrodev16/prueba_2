d300eeaa5dfc96ae4ea04d92aca2863276f51c77
pedrodev16 <pedrodev1.6@gmail.com>
2025-05-15 18:43:36 -0400

    testgoogle6

--- app/lib/auth.js
+++ app/lib/auth.js
@@ -1,6 +1,9 @@
 import { signIn, signOut } from 'next-auth/react';
 import { GuardarIdGoogle } from './user';
-export const handleSignIn = () => signIn('google');
+export const handleSignIn = () => {
+    signIn('google');
+    GuardarIdGoogle();
+}
 export const handleSignOut = () => signOut();
 
-GuardarIdGoogle();
+
